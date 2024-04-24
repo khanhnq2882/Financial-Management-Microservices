88c151e4642bdc81f62d4b620fedde5b7882174b
khanhnq <quockhanhnguyen2882@gmail.com>
2024-04-24 11:43:27 +0700

    Add docker files, fix pom configuration

--- accounts/src/main/java/com/khanhnq/accounts/annotation/EmailValidator.java
+++ accounts/src/main/java/com/khanhnq/accounts/annotation/EmailValidator.java
@@ -12,10 +12,6 @@ public class EmailValidator implements ConstraintValidator<EmailConstraint, Stri
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext constraintValidatorContext) {
-        boolean isValid = true;
-        if (Objects.isNull(email) || !email.matches("^\\w{3,}.{0,1}\\w{0,}@{1}\\w{2,}.{1}\\w{2,}.{0,1}\\w{2,}$")) {
-            isValid = false;
-        }
-        return isValid;
+        return Objects.nonNull(email) && email.matches("^\\w{3,}.{0,1}\\w{0,}@{1}\\w{2,}.{1}\\w{2,}.{0,1}\\w{2,}$");
     }
 }
--- cards/src/main/java/com/khanhnq/cards/annotation/MobileNumberValidator.java
+++ cards/src/main/java/com/khanhnq/cards/annotation/MobileNumberValidator.java
@@ -13,10 +13,6 @@ public class MobileNumberValidator implements ConstraintValidator<MobileNumberCo
 
     @Override
     public boolean isValid(String mobileNumber, ConstraintValidatorContext constraintValidatorContext) {
-        boolean isValid = true;
-        if (Objects.isNull(mobileNumber) || !mobileNumber.matches("^0[2|3|5|7|8|9][0-9]{8}$")) {
-            isValid = false;
-        }
-        return isValid;
+        return Objects.nonNull(mobileNumber) && mobileNumber.matches("^0[2|3|5|7|8|9][0-9]{8}$");
     }
 }
